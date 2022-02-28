15263120f93a038feffacba492581dbe6c0e38e9
AaronLyons1 <lyonsaaron02@gmail.com>
2022-02-28 16:12:57 +0000

    started js

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultipylQuestion(){
+    
+}
+
+function displayDivideQuestion(){
+    
+}
