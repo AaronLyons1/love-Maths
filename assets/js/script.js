9bf8709483adc5e0d381397a5080dfae2bc17122
AaronLyons1 <lyonsaaron02@gmail.com>
2022-02-28 16:22:55 +0000

    add event listeners for start of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+           if (this.getAttribute("data-type") === "submit"){
+            alert("You Clicked Submit!")
+           } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You Clicked ${gameType}`);
+           }
+        })
+    }
+})
+
 function runGame(){
 
 }
@@ -33,3 +48,4 @@ function displayMultipylQuestion(){
 function displayDivideQuestion(){
     
 }
+
